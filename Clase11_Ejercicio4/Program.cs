d3184d627860d1742ff2c8438a015c2adcfe4062
Ivan Lopez <ialmendra@infobae.com>
2025-10-19 21:30:20 -0300

    Agregar archivos de proyecto.

---
+++ Clase11_Ejercicio4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+
+public class Numeros : IEnumerable, IEnumerator
+{
+    private int[] _items = { 10, 20, 30, 40, 50 };
+    private int _position = -1;
+
+    // Propiedad de solo lectura. Retorna el elemento que se está recorriendo.
+    public object Current
+    {
+        get { return _items[_position]; }
+    }
+
+    // Método. Determina que elemento se va a colocar en Current y retorna verdadero.Si ya no hay nada por recorrer debe retornar falso.
+    public bool MoveNext()
+    {
+        _position++;
+        return _position < _items.Length;
+    }
+
+    // Método. Se inicializan valores que se utilizan en la iteración.
+    public void Reset()
+    {
+        _position = -1;
+    }
+
+    // IEnumerable - Retorna el enumerador (esta misma clase)
+    public IEnumerator GetEnumerator()
+    {
+        Reset();
+        return this;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Numeros numeros = new Numeros();
+
+        Console.WriteLine("Recorriendo con foreach:");
+        foreach (var n in numeros)
+        {
+            Console.WriteLine($"Número: {n}");
+        }
+    }
+}
